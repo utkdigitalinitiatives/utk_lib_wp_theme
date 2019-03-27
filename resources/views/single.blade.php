46ec9c1ce28daa7e7d42a72939f484bbc9c24ec4
Mat Jordan <mat@utk.edu>
2019-03-27 12:44:50 -0400

    Add class for post style rendering.

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -2,12 +2,13 @@
 
 @php
     $postStyle = get_site_option('options_site_post_style');
+    $postStyleClass = sanitize_html_class(strtolower($postStyle));
 @endphp
 
 @section('content')
     @while(have_posts()) @php the_post() @endphp
     @include('partials.page-header')
-        <div class="page-body">
+        <div class="page-body @if($postStyleClass)page-body-{{$postStyleClass}}@endif">
 
             @if('Blog' == $postStyle)
                 @include('partials.sections.main-full-width')
