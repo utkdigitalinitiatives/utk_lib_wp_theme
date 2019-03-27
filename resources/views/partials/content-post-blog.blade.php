494889ca857508f2e7c4593d3bfbfaf6c861b4f9
Mat Jordan <mat@utk.edu>
2019-03-27 16:24:18 -0400

    Update blog title styling and meta info.

--- resources/views/partials/content-post-blog.blade.php
+++ resources/views/partials/content-post-blog.blade.php
@@ -2,17 +2,26 @@
 
 Namespace App\Controllers;
 
-$avatar     = get_avatar_data(get_post_meta('author'));
-$author     = get_the_author_meta('display_name');
-$estimate   = App::estimateReadTime($post->post_content)
-
 @endphp
 <div class="page-body--content--inner">
     <div class="page-body--content--title">
+        @php
+            $dateline2   = $post->post_modified;
+            $dateline   = date('F j, Y', strtotime($post->post_date));
+            $avatar     = get_avatar_data(get_post_meta($post->post_author));
+            $author     = get_the_author_meta('display_name', $post->post_author);
+            $estimate   = App::estimateReadTime($post->post_content)
+        @endphp
         <h1>@php echo get_the_title(); @endphp</h1>
-        <img src="{{$avatar['url']}}" />
-        {{$author}}
-        {{$estimate}} minute read
+
+        <div>
+            <div>{{$author}}</div>
+            <div>
+                <span>{{$dateline}}</span>
+                <span>{{$estimate}} minute read</span>
+            </div>
+        </div>
+
     </div>
     <div class="page-body--content--body">
         @php the_content() @endphp
