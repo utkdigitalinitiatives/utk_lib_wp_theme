f02bf2df2628350c245b3a00773cf3b0cd12bb59
Mat Jordan <mat@utk.edu>
2019-03-27 12:35:41 -0400

    Add theme layer conditional for post type layout.

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,10 +1,25 @@
 @extends('layouts.app')
 
+@php
+    $postStyle = get_site_option('options_site_post_style');
+@endphp
+
 @section('content')
     @while(have_posts()) @php the_post() @endphp
     @include('partials.page-header')
-    <div class="page-body">
-        @include('partials.sections.main-full-width')
-    </div>
+        <div class="page-body">
+
+            @if('Blog' == $postStyle)
+                @include('partials.sections.main-full-width')
+
+            @elseif('Standard' === $postStyle)
+                @include('partials.sections.main')
+
+            @else
+                @include('partials.sections.main')
+
+            @endif
+
+        </div>
     @endwhile
 @endsection
