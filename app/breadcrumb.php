5c89f5d90f9a2ac51fd85ce09cb2f3f8cc6cc856
Mat Jordan <mat@utk.edu>
2019-03-28 15:23:36 -0400

    Add Breadcrumb for sites not using subsite location.

--- app/breadcrumb.php
+++ app/breadcrumb.php
@@ -5,33 +5,49 @@ namespace App;
 class SubsiteBreadcumb
 {
 
-    public static function breadcrumbInline($theme_location = 'subsite')
+    public static function breadcrumbInline($theme_location = 'sidebar_subsite')
     {
-        if (function_exists('get_blog_details')) {
-             $details = get_blog_details();
-        } else {
+        $locations = get_nav_menu_locations();
+        $subsite_menu = wp_get_nav_menu_object($locations[$theme_location]);
+
+        if (function_exists('get_blog_details')) :
+            $details = get_blog_details();
+        else :
             $details = null;
-        }
-
-        $items = wp_get_nav_menu_items($theme_location);
-
-        if ($items) :
-            _wp_menu_item_classes_by_context($items); // Set up the class variables, including current-classes
-
-            $crumbs = [
-                '<a href="' . network_site_url() .'">Libraries</a>',
-                '<a href="' . $details->siteurl . '">' . $details->blogname  .'</a>'
-            ];
-
-            foreach ($items as $item) {
-                if ($item->current_item_ancestor) {
-                    $crumbs[] = "<a href=\"{$item->url}\" title=\"{$item->title}\">{$item->title}</a>";
-                } elseif ($item->current) {
-                    $crumbs[] = "<span class='current-menu-item'>{$item->title}</span>";
-                }
-            }
-
-            echo implode('<span class="icon-angle-right"></span>', $crumbs);
         endif;
+
+        if ($subsite_menu) :
+            $items = wp_get_nav_menu_items($subsite_menu);
+            _wp_menu_item_classes_by_context($items);
+
+            self::breadcrumbRender($details, $items);
+        else :
+            self::breadcrumbRender($details, null);
+        endif;
+
+        return;
+    }
+
+    public static function breadcrumbGetTrunk($details)
+    {
+        return [
+            '<a href="' . network_site_url() .'">Libraries</a>',
+            '<a href="' . $details->siteurl . '">' . $details->blogname  .'</a>'
+        ];
+    }
+
+    public static function breadcrumbRender($details, $items)
+    {
+        $crumbs = self::breadcrumbGetTrunk($details);
+
+        foreach ($items as $item) :
+            if ($item->current_item_ancestor) :
+                $crumbs[] = "<a href=\"{$item->url}\" title=\"{$item->title}\">{$item->title}</a>";
+            elseif ($item->current) :
+                $crumbs[] = "<span class='current-menu-item'>{$item->title}</span>";
+            endif;
+        endforeach;
+
+        echo implode('<span class="icon-angle-right"></span>', $crumbs);
     }
 }
