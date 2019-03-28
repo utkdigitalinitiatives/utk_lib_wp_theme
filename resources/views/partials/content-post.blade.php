c2e87cea48bd53b03f734d16c48216445d216032
Mat Jordan <mat@utk.edu>
2019-03-28 15:31:45 -0400

    Fix blade rendering for post teaser titles.

--- resources/views/partials/content-post.blade.php
+++ resources/views/partials/content-post.blade.php
@@ -1,6 +1,8 @@
 <article @php post_class() @endphp>
   <header>
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h2>
+    <h2 class="entry-title">
+      <a href="@php the_permalink() @endphp">@php the_title() @endphp</a>
+    </h2>
   </header>
   <div class="entry-summary">
     @php the_excerpt() @endphp
