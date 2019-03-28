62e6564ab92aa434660032957e750530aef3eec0
Mat Jordan <mat@utk.edu>
2019-03-28 17:12:19 -0400

    Fix wrapper structure.

--- resources/views/partials/sections/main-archive.blade.php
+++ resources/views/partials/sections/main-archive.blade.php
@@ -3,18 +3,18 @@
     @include('partials.components.breadcrumb')
     <div class="page-body--flex">
         <main class="page-body--content">
+            @if (!have_posts())
+                <div class="alert alert-warning">
+                    {{ __('Sorry, no results were found.', 'sage') }}
+                </div>
+                {!! get_search_form(false) !!}
+            @endif
             <div class="facetwp-template">
-                @if (!have_posts())
-                    <div class="alert alert-warning">
-                        {{ __('Sorry, no results were found.', 'sage') }}
-                    </div>
-                    {!! get_search_form(false) !!}
-                @endif
                 @while (have_posts()) @php the_post() @endphp
-                @include('partials.content-'.get_post_type())
+                    @include('partials.content-'.get_post_type())
                 @endwhile
-                {!! get_the_posts_navigation() !!}
             </div>
+            {!! get_the_posts_navigation() !!}
         </main>
         <aside class="page-body--aside">
             @php
