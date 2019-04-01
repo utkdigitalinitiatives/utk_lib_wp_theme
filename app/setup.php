86c206259d765a8faa2192afb2d8cb7288616ae3
Mat Jordan <mat@utk.edu>
2019-04-01 14:38:00 -0400

    Format image resize filter to PSR2 standards.

--- app/setup.php
+++ app/setup.php
@@ -57,16 +57,20 @@ add_filter('upload_dir', function ($upload) {
 /**
  * Image Sizes
  */
-add_image_size('post-thumbnail', 400, 400, TRUE);
-add_image_size('post-thumbnail-pre', 11, 11, TRUE);
+add_image_size('post-thumbnail', 400, 400, true);
+add_image_size('post-thumbnail-pre', 11, 11, true);
 add_image_size('hero', 1364, 521, false);
 
 /**
  *  Upscale smaller than ascribed dimension images
  */
 
-add_filter( 'image_resize_dimensions', function ( $default, $orig_w, $orig_h, $new_w, $new_h, $crop ){
-    if ( !$crop ) return null; // let the wordpress default function handle this
+add_filter('image_resize_dimensions', function ($default, $orig_w, $orig_h, $new_w, $new_h, $crop) {
+
+    // let the wordpress default function handle this
+    if (!$crop) :
+        return null;
+    endif;
 
     $aspect_ratio = $orig_w / $orig_h;
     $size_ratio = max($new_w / $orig_w, $new_h / $orig_h);
@@ -74,11 +78,11 @@ add_filter( 'image_resize_dimensions', function ( $default, $orig_w, $orig_h, $n
     $crop_w = round($new_w / $size_ratio);
     $crop_h = round($new_h / $size_ratio);
 
-    $s_x = floor( ($orig_w - $crop_w) / 2 );
-    $s_y = floor( ($orig_h - $crop_h) / 2 );
+    $s_x = floor(($orig_w - $crop_w) / 2);
+    $s_y = floor(($orig_h - $crop_h) / 2);
 
     return array( 0, 0, (int) $s_x, (int) $s_y, (int) $new_w, (int) $new_h, (int) $crop_w, (int) $crop_h );
-}, 10, 6 );
+}, 10, 6);
 
 /**
  * Theme setup
