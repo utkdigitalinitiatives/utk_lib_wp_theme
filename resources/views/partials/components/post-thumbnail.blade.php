32832dba96fc873f95c02e6417bffa0b71d8ac43
Mat Jordan <mat@utk.edu>
2019-04-01 13:46:59 -0400

    Fixing FacetWP toggle bug for lazyloaded images.

--- resources/views/partials/components/post-thumbnail.blade.php
+++ resources/views/partials/components/post-thumbnail.blade.php
@@ -4,22 +4,19 @@
  * Build thumbnail for rendering with LazyLoad srcset if available.
  */
 
-$id     = get_post_thumbnail_id();
-$pre    = wp_get_attachment_image_src($id, 'post-thumbnail-pre')[0];
-$thumb  = wp_get_attachment_image_src($id, 'post-thumbnail')[0];
-$alt    = get_post_meta($id, '_wp_attachment_image_alt')[0];
+$id         = get_post_thumbnail_id();
+$img        = get_the_post_thumbnail();
+$pre        = wp_get_attachment_image_src($id, 'post-thumbnail-pre')[0];
+$alt        = get_post_meta($id, '_wp_attachment_image_alt')[0];
 
 @endphp
 
 <div class="article--thumbnail">
     @if( is_user_logged_in() )
-        <img src="@php echo $thumb @endphp"
-             alt="@php echo $alt @endphp" />
+        @php echo $img; @endphp
         @else
         <div class="article--thumbnail--pre"
              style="background-image: url('@php echo $pre @endphp');"></div>
-        <img src="@php echo $pre @endphp"
-             data-lazy-src="@php echo $thumb @endphp"
-             alt="@php echo $alt @endphp" />
+        @php echo $img; @endphp
     @endif;
 </div>
