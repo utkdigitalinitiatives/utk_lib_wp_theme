8e397d939a3c6cc20384d2e4fc245822a36d1417
Mat Jordan <mat@utk.edu>
2019-04-01 12:52:06 -0400

    Add real alt attribute values to post thumbs.

--- resources/views/partials/components/post-thumbnail.blade.php
+++ resources/views/partials/components/post-thumbnail.blade.php
@@ -4,15 +4,22 @@
  * Build thumbnail for rendering with LazyLoad srcset if available.
  */
 
-$id  = get_post_thumbnail_id();
-$pre = wp_get_attachment_image_src($id, 'post-thumbnail-pre');
-$thumb = wp_get_attachment_image_src($id, 'post-thumbnail');
+$id     = get_post_thumbnail_id();
+$pre    = wp_get_attachment_image_src($id, 'post-thumbnail-pre')[0];
+$thumb  = wp_get_attachment_image_src($id, 'post-thumbnail')[0];
+$alt    = get_post_meta($id, '_wp_attachment_image_alt')[0];
 
 @endphp
 
 <div class="article--thumbnail">
-    <div class="article--thumbnail--pre" style="background-image: url('@php echo $pre[0] @endphp');"></div>
-    <img src="@php echo $pre[0] @endphp"
-         data-lazy-src="@php echo $thumb[0] @endphp"
-         alt="this and that" />
+    @if( is_user_logged_in() )
+        <img src="@php echo $thumb @endphp"
+             alt="@php echo $alt @endphp" />
+        @else
+        <div class="article--thumbnail--pre"
+             style="background-image: url('@php echo $pre @endphp');"></div>
+        <img src="@php echo $pre @endphp"
+             data-lazy-src="@php echo $thumb @endphp"
+             alt="@php echo $alt @endphp" />
+    @endif;
 </div>
