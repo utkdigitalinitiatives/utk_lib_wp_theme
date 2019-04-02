3b7112ba145c4a6d220b97cc357df959f462488d
Mat Jordan <mat@utk.edu>
2019-04-02 10:22:14 -0400

    Add basic paging to archive pages.

--- resources/views/partials/sections/main-archive.blade.php
+++ resources/views/partials/sections/main-archive.blade.php
@@ -3,32 +3,34 @@
     @include('partials.components.breadcrumb')
     <div class="page-body--flex">
         <main class="page-body--content">
-            @if (!have_posts())
-                <div class="alert alert-warning">
-                    {{ __('Sorry, no results were found.', 'sage') }}
-                </div>
-                {!! get_search_form(false) !!}
-            @endif
             <div class="facetwp-template">
-                @while (have_posts()) @php the_post() @endphp
-                    @include('partials.content-'.get_post_type())
-                @endwhile
+                @if (!have_posts())
+                    <div class="alert alert-warning">
+                        {{ __('Sorry, no results were found.', 'sage') }}
+                    </div>
+                @else
+                    @while (have_posts()) @php the_post() @endphp
+                        @include('partials.content-'.get_post_type())
+                    @endwhile
+                @endif
             </div>
-            {!! get_the_posts_navigation() !!}
+            <div class="facetwp-pager">
+                @php echo facetwp_display('pager') @endphp
+            </div>
         </main>
         <aside class="page-body--aside">
             <div class="page-body--aside--facets">
                 <div class="page-body--aside--facets--item">
                     <h4>Search Articles</h4>
-                    @php echo facetwp_display( 'facet', 'post_search' ); @endphp
+                    @php echo facetwp_display('facet', 'post_search'); @endphp
                 </div>
                 <div class="page-body--aside--facets--item">
                     <h4>Filter by Date</h4>
-                    @php echo facetwp_display( 'facet', 'post_month_year' ); @endphp
+                    @php echo facetwp_display('facet', 'post_month_year'); @endphp
                 </div>
                 <div class="page-body--aside--facets--item">
                     <h4>Filter by Category</h4>
-                    @php echo facetwp_display( 'facet', 'post_categories' ); @endphp
+                    @php echo facetwp_display('facet', 'post_categories'); @endphp
                 </div>
             </div>
             @include('partials.sidebar')
