acace09438158bdb7addbfc7a74b0d68f54327aa
Mat Jordan <mat@utk.edu>
2019-04-15 11:40:08 -0400

    Add switch for callout template rendering.

--- resources/views/partials/sections/callout.blade.php
+++ resources/views/partials/sections/callout.blade.php
@@ -1,7 +1,6 @@
 @php
   $callout = UTKLibrary\Library\Models\Callout::render_callout();
 @endphp
-
 @if($callout)
   @php
     $type = $callout['fields']['callout_type'];
@@ -9,9 +8,36 @@
   <section class="utk-section section-callout section-callout--{{$type}}">
     <div class="container">
       <div class="utk-callout">
-        @if ($type == 'background_video')
+        @switch($type)
+
+          @case('image-text')
+            @include('partials.components.callout-image-text')
+            @break
+
+          @case('image-background')
+            @include('partials.components.callout-image-background')
+            @break
+
+          @case('image-gallery')
+            @include('partials.components.callout-image-gallery')
+            @break
+
+          @case('image-slider')
+            @include('partials.components.callout-image-slider')
+            @break
+
+          @case('video-text')
+            @include('partials.components.callout-video-text')
+            @break
+
+          @case('video-background')
             @include('partials.components.callout-video-background')
-        @endif
+            @break
+
+          @default
+            @include('partials.components.callout-default')
+
+        @endswitch
       </div>
     </div>
   </section>
