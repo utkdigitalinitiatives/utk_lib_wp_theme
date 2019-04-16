c5829bb595a388877d011178b3452d336d4820f6
Mat Jordan <mat@utk.edu>
2019-04-16 08:16:34 -0400

    Fix nested array bug related ACF groups on callouts.

--- resources/views/partials/components/callout-image-text.blade.php
+++ resources/views/partials/components/callout-image-text.blade.php
@@ -3,11 +3,13 @@
     $image_post_id = $callout['fields']['callout_media']['callout_image']['id'];
     $image  = wp_get_attachment_image($image_post_id, $size);
     $orientation = $callout['fields']['callout_orientation'];
+
+    print_r ($callout);
 @endphp
 <div class="utk-callout--inner utk-callout--orientation-{{$orientation}}">
     <div class="utk-callout--content">
-        <h3>@php echo $callout['fields']['callout_headline']; @endphp</h3>
-        <div class="utk-callout--content--paragraphs">@php echo $callout['fields']['callout_body']; @endphp</div>
+        <h3>@php echo $callout['fields']['callout_content']['callout_headline']; @endphp</h3>
+        <div class="utk-callout--content--paragraphs">@php echo $callout['fields']['callout_content']['callout_body']; @endphp</div>
     </div>
     <figure class="utk-callout--image">@php echo $image @endphp</figure>
 </div>
