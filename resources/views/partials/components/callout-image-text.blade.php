f587df2d74bbe60973f587d11a484df801069463
Mat Jordan <mat@utk.edu>
2019-04-16 09:48:41 -0400

    Add responsiveness and clipping mask to on callouts.

--- resources/views/partials/components/callout-image-text.blade.php
+++ resources/views/partials/components/callout-image-text.blade.php
@@ -1,10 +1,13 @@
 @php
+    // styling
+    $orientation = $callout['fields']['callout_orientation'];
+    $style = $callout['fields']['callout_style'];
+    // media
     $size = 'callout_image';
     $image_post_id = $callout['fields']['callout_media']['callout_image']['id'];
     $image  = wp_get_attachment_image($image_post_id, $size);
-    $orientation = $callout['fields']['callout_orientation'];
 @endphp
-<div class="utk-callout--inner utk-callout--orientation-{{$orientation}}">
+<div class="utk-callout--inner utk-callout--style-{{$style}} utk-callout--orientation-{{$orientation}}">
     <div class="utk-callout--content">
         <h3>@php echo $callout['fields']['callout_content']['callout_headline']; @endphp</h3>
         <div class="utk-callout--content--paragraphs">@php echo $callout['fields']['callout_content']['callout_body']; @endphp</div>
