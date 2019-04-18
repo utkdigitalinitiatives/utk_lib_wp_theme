bcbfdd3ad1bb99bd695b52f9ed3af96b9ccdd11c
Mat Jordan <mat@utk.edu>
2019-04-18 11:43:55 -0400

    Base styling for onesearch component.

--- resources/views/partials/components/onesearch.blade.php
+++ resources/views/partials/components/onesearch.blade.php
@@ -4,25 +4,43 @@
         24, // agvet
         25  // music
     ];
+    $onesearchLogo = \App\asset_path('images/ut-onesearch.svg');
 @endphp
 @if (is_front_page() && in_array(get_current_blog_id(), $allowedSites))
     <div class="utk-onesearch">
-        <a href="https://www.lib.utk.edu/onesearch"><img src="https://www.lib.utk.edu/template/2016/images-utlibraries/onesearch-215.png" class="onesearch-logo" alt="Click to sign in and access OneSearch" title="Click to sign in and access OneSearch" width="215" height="50" /></a>
-        <div class="search-box">
-            <div class="search-link-box">
-                <div class="search-link">
+        <div class="utk-onesearch--header">
+            <a href="https://www.lib.utk.edu/onesearch" class="utk-onesearch--header--logo">
+                <img src="{{$onesearchLogo}}"
+                     alt="Click to sign in and access OneSearch"
+                />
+            </a>
+            <div class="utk-onesearch--header--links">
+                <div class="utk-onesearch--header--links--item">
                     <a href="https://utk-almaprimo.hosted.exlibrisgroup.com/primo-explore/search?vid=01UTK&mode=advanced" target="_blank">Advanced Search</a>
                 </div>
-                <div class="search-link">
-                    <a href="https://utk-almaprimo.hosted.exlibrisgroup.com/primo-explore/browse?vid=01UTK" target="_blank">Browse UT Collections</a></div>
+                <div class="utk-onesearch--header--links--item">
+                    <a href="https://utk-almaprimo.hosted.exlibrisgroup.com/primo-explore/browse?vid=01UTK" target="_blank">Browse UT Collections</a>
+                </div>
             </div>
-            <form accept-charset="UTF-8" action="/onesearch.php" method="post"><input class="form-control searchfield" title="search" name="box" type="text" onfocus="if(this.value == 'Search for books, articles, media, digital collections . . .') { this.value = ''; }" value="Search for books, articles, media, digital collections . . ." width="100%" />
-
-                <div class="search-options">
+        </div>
+        <div class="utk-onesearch--box">
+            <form accept-charset="UTF-8" action="/onesearch.php" method="post">
+                <input class="form-control utk-onesearch--box--search"
+                       title="search"
+                       type="text"
+                       value=""
+                       placeholder="Search for books, articles, media, digital collections . . ."
+                       width="100%" />
+                <div class="utk-onesearch--box--options">
                     Limit search to: &nbsp; <input checked="checked" name="searchtype" type="radio" value="onesearch" hidden="yes" />
-                    <span class="search-option"><input name="searchtype" type="radio" value="utcollections" /> UT Collections</span>
-                    <span class="search-option"> <input name="searchtype" type="radio" value="coursereserves" /> Course Reserves</span>
-                    <br class="clear" />
+                    <span class="utk-onesearch--box--options--item">
+                        <input name="searchtype" type="radio" value="utcollections" />
+                        UT Collections
+                    </span>
+                    <span class="utk-onesearch--box--options--item">
+                        <input name="searchtype" type="radio" value="coursereserves" />
+                        Course Reserves
+                    </span>
                 </div>
             </form>
         </div>
