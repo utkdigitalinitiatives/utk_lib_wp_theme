eabeec84c5efeefc03390aa0b90ba260adb061ce
Mat Jordan <mat@utk.edu>
2019-04-23 16:11:51 -0400

    Add functionality for swapping elements for scroll to top on subsite help.

--- resources/assets/scripts/components/Subsite.js
+++ resources/assets/scripts/components/Subsite.js
@@ -1,11 +1,25 @@
 /* Subsite */
 
 export default class Subsite {
+
   constructor() {
     this.subsiteListener();
   }
 
   subsiteListener () {
+
+    var subsiteContainer = document.getElementsByClassName("subsite-menu-wrap");
+
+    for (var i = 0; i < subsiteContainer.length; i++) {
+      subsiteContainer[i].addEventListener('scroll', this.trackScrollingSubsite, false);
+    }
+
+    $('#subsite-menu-top').click(function() {
+      console.log('x');
+      $('.subsite-menu-wrap').animate(
+          { scrollTop: 0 }, 290
+      );
+    });
 
     $( "#page-header-subsite-menu" ).mouseenter(function() {
       $( "body" ).addClass( "subsite-menu-dropdown-expand" );
@@ -23,5 +37,27 @@ export default class Subsite {
       $( "body" ).removeClass( "subsite-menu-mobile-expand" );
     });
 
+  }
+
+  static getBounds(el) {
+    return el.getBoundingClientRect();
+  }
+
+  trackScrollingSubsite() {
+
+    let subsiteScroll = document.getElementById('subsite-scroll-trigger');
+    let subsiteScrollBounds = Subsite.getBounds(subsiteScroll);
+
+    let help = document.getElementById('subsite-menu-help');
+    let helpTop = document.getElementById('subsite-menu-top');
+    let helpBounds = Subsite.getBounds(help);
+
+    if (helpBounds.top > subsiteScrollBounds.top) {
+      $( help ).addClass( "subsite-menu-help--scrolled" );
+      $( helpTop ).addClass( "subsite-menu-help--scrolled" );
+    } else {
+      $( help ).removeClass( "subsite-menu-help--scrolled" );
+      $( helpTop ).removeClass( "subsite-menu-help--scrolled" );
+    }
   }
 }
