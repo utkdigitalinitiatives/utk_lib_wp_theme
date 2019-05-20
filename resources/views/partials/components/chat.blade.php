a96f015840bbf98431edc9c3a8c2ab8b26f5727f
Mat Jordan <mat@utk.edu>
2019-05-20 17:00:04 -0400

    Markout old chat button.

--- resources/views/partials/components/chat.blade.php
+++ resources/views/partials/components/chat.blade.php
@@ -1,6 +1,9 @@
-<a href="#" class="options--item options--item-chat" id="option-header-chat" data-event="option-chat-expand">
-  <span class="options--item--title" data-event="option-chat-expand">Chat</span>
-  <span class="options--item--icon" data-event="option-chat-expand">
-    <span class="icon-chat" data-event="option-chat-expand"></span>
-  </span>
-</a>
+<div id="libchat_cbb3e39658d3f35979d91f22200d30f3">
+{{--<a href="#" class="options--item options--item-chat" id="option-header-chat" data-event="option-chat-expand">--}}
+  {{--<span class="options--item--title" data-event="option-chat-expand">Chatx</span>--}}
+  {{--<span class="options--item--icon" data-event="option-chat-expand">--}}
+    {{--<span class="icon-chat" data-event="option-chat-expand"></span>--}}
+  {{--</span>--}}
+{{--</a>--}}
+</div>
+<script type="text/javascript" src="https://v2.libanswers.com/load_chat.php?hash=cbb3e39658d3f35979d91f22200d30f3"></script>
