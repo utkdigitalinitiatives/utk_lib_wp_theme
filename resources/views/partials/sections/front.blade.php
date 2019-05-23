40e983e200fdfac722f5f5bdde2d71987a3357d7
Mat Jordan <mat@utk.edu>
2019-05-23 11:51:31 -0400

    Updated header with Google Maps integration.

--- resources/views/partials/sections/front.blade.php
+++ resources/views/partials/sections/front.blade.php
@@ -1,6 +1,6 @@
-<link
-        rel="stylesheet"
-        href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
-/>
+{{--<link--}}
+        {{--rel="stylesheet"--}}
+        {{--href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"--}}
+{{--/>--}}
 <div id="utk-panel"></div>
 <div id="utk-placeholder"></div>
