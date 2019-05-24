1750cef804386244d962e72b3b34bb5831ede4d8
Mat Jordan <mat@utk.edu>
2019-05-24 16:43:51 -0400

    Add conditionals for GET var, further style refinements.

--- resources/views/partials/components/google-custom-search.blade.php
+++ resources/views/partials/components/google-custom-search.blade.php
@@ -6,23 +6,27 @@
 @if (is_front_page() && in_array(get_current_blog_id(), $allowedSites))
     <form class="utk-gcs">
         <div class="utk-gcs--bar">
-        <input name="gcs"
-               type="text"
-               placeholder="Find Services, News, and More"
-               value="@php echo $_GET['q'] @endphp"
-               class="utk-gcs--search" />
-        <input type="submit"
-               value="Go"
-               class="utk-gcs--submit" />
+            <input name="gcs"
+                   type="text"
+                   placeholder="Find Services, News, and More"
+                   value="@php echo $_GET['gcs'] @endphp"
+                   class="utk-gcs--search" />
+            @if(isset($_GET['gcs']))
+                <input type="submit"
+                       value="Search lib.utk.edu"
+                       class="utk-gcs--submit" />
+            @endif
         </div>
-        <div class="utk-gcs--buttons">
-            <input type="submit"
-                   value="Search lib.utk.edu"
-                   class="utk-gcs--submit" />
-            <input type="submit"
-                   value="Switch to OneSearch"
-                   class="utk-gcs--submit" />
-        </div>
+        @if(!isset($_GET['gcs']))
+            <div class="utk-gcs--buttons">
+                <input type="submit"
+                       value="Search lib.utk.edu"
+                       class="utk-gcs--submit" />
+                <input type="submit"
+                       value="Switch to OneSearch"
+                       class="utk-gcs--submit" />
+            </div>
+        @endif
     </form>
     <script>
         (function() {
