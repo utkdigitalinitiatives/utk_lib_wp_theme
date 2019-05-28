8378af1d6e06fe0fe5c9767e1b35978db13c08ea
Mat Jordan <mat@utk.edu>
2019-05-28 10:44:47 -0400

    Add graceful SEO friendly hiding of page title.

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -1,4 +1,8 @@
-<div class="page-body--content--inner">
+@if(get_field('page_hide_title'))
+    <div class="page-body--content--inner page-body--hide-title">
+@else
+    <div class="page-body--content--inner">
+@endif
   @if(get_the_title())
     <div class="page-body--content--title">
       <h1>@php echo get_the_title(); @endphp</h1>
