50b6f0643bdd71fa262a24a527eacd831aa49258
Mat Jordan <mat@utk.edu>
2019-05-28 15:45:12 -0400

    Add functionality for OneSearch passing on search submission.

--- app/Controllers/Search.php
+++ app/Controllers/Search.php
@@ -13,14 +13,34 @@ class Search extends Controller
             if ($_POST['method'] === 'libraries') :
                 wp_redirect('search?gcs=' . $_POST['value']);
             elseif ($_POST['method'] === 'onesearch') :
-                print '<pre>';
-                print_r ($_POST);
-                print '</pre>';
+                self::handleOneSearch();
             endif;
         else :
             return;
         endif;
 
         return;
+    }
+
+    public function handleOneSearch()
+    {
+        // base url for primo
+        $url="https://utk-almaprimo.hosted.exlibrisgroup.com/primo-explore";
+        $url.="/search?vid=01UTK&institution=01UTK";
+        $url.="&facet=rtype,exclude,reviews&facet=rtype,exclude,reference_entrys";
+        $url.="&search_scope=OneSearch&tab=default_tab&onCampus=false&group=GUEST";
+
+        // sanitize before passing to primo
+        if (isset($_POST['value'])) {
+            $box=urlencode($_POST['value']);
+            $box=trim($box);
+            $box=htmlentities($box);
+            $box=stripslashes($box);
+            $bx=$box;
+        }
+
+        $url .= "&query=any,contains,$bx";
+        header("Location: $url");
+        exit();
     }
 }
