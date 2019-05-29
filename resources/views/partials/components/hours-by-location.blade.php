04f11736948d50bd420eae2b0edd97d11ebfbf11
Mat Jordan <mat@utk.edu>
2019-05-29 11:09:55 -0400

    Update hours component to better match LibCal api schema.

--- resources/views/partials/components/hours-by-location.blade.php
+++ resources/views/partials/components/hours-by-location.blade.php
@@ -1,29 +1,40 @@
 @php
 
+// get lid for location
+$location = get_option('options_site_hours_site_hours_lid');
+
 $base = 'https://api3.libcal.com/api_hours';
 $type = 'today.php';
 $iid = '968'; // UT Libraries iid
 $format = 'json';
 
-$url = $base . '_' . $type . '?iid=' . $iid . '&format=' . $format;
-
-// get lid for location
-$location = get_option('options_site_hours_site_hours_lid');
+$url = $base . '_' . $type . '?iid=' . $iid . '&format=' . $format . '&lid=' . $location ;
 
 @endphp
 
-@if (strlen($url)!=0 && $location > -1 && $location < 8)
+@if (strlen($url)!=0 && $location >= 0)
 
     @php
         $response = json_decode(file_get_contents($url));
         $headers = $http_response_header;
         $hoursPage = get_permalink(get_option('options_site_hours_site_hours_page'));
-        $hoursRange = $response->locations[$location]->rendered;
+        $hoursRange = $response->locations[0]->rendered;
         $hoursImage = get_field('site_hours_site_hours_background_image', 'option');
         $hoursImageRender = wp_get_attachment_image_src($hoursImage['ID'], 'medium')[0];
     @endphp
 
-    @if ($headers[0] === 'HTTP/1.1 200 OK')
+    @if ($headers[0] === 'HTTP/1.1 200 OK' && isset($response->locations))
+        @php
+
+            $hoursRangeClass = 'utk-hours--today--range';
+
+            if ($response->locations[0]->times->currently_open) {
+                $hoursRangeClass .= ' utk-hours--open';
+            } else {
+                $hoursRangeClass .= 'utk-hours--closed';
+            }
+
+        @endphp
         <section class="utk-hours-section">
             <div class="container">
                 <div class="utk-hours--wrap">
@@ -31,7 +42,7 @@ $location = get_option('options_site_hours_site_hours_lid');
                         <span class="utk-hours--today--label">
                             Hours Today
                         </span>
-                        <span class="utk-hours--today--range">
+                        <span class="{{$hoursRangeClass}}">
                             @php echo $hoursRange @endphp
                         </span>
                     </a>
