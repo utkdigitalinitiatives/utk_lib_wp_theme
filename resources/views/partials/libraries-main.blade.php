4d31aa85b689abe3cf22ef6ce79484b2c820cb1c
Mat Jordan <mat@utk.edu>
2019-05-29 12:29:24 -0400

    Update conditional to check if current WP is multisite.

--- resources/views/partials/libraries-main.blade.php
+++ resources/views/partials/libraries-main.blade.php
@@ -1,6 +1,6 @@
 <div id="utk-lib-main" role="document">
     <section class="utk-section section-main content">
-        @if (is_main_site() && is_front_page())
+        @if (is_multisite() && is_main_site() && is_front_page())
             @include('partials.sections.front')
         @else
             @yield('content')
