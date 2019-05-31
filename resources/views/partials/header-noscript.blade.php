d07fe2e778c02c9cef32c0b75b3fe28de5a6084c
Mat Jordan <mat@utk.edu>
2019-05-31 10:17:11 -0400

    Add noscript fallback for React header.

---
+++ resources/views/partials/header-noscript.blade.php
@@ -0,0 +1,59 @@
+<noscript>
+    <div class="utk-lib-header-noscript">
+        <header class="utk-header">
+            <div class="utk-header-main">
+                <div class="container">
+                    <div class="utk-logo-flex">
+                        <div id="utk-logo" class="utk-logo-wrapper">
+                            <a href="https://www.utk.edu" tabindex="1">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48.24 48.24"
+                                     class="utk-logo utk-logo-square">
+                                    <title>University of Tennessee Libraries=</title>
+                                    <polygon class="utk-logo--orange"
+                                             points="47.999 47.999 47.999 0 0 0 0 47.999 47.999 47.999 47.999 47.999"></polygon>
+                                    <path class="utk-logo--white"
+                                          d="M41.941,6.054V18.165H37.456c0-2.231-2.208-4.037-4.934-4.037a5.088,5.088,0,0,0-4.485,2.355V31.62a5.721,5.721,0,0,0,5.592,5.83h.014v4.486H14.357V37.45h.013a5.721,5.721,0,0,0,5.593-5.83V16.483a5.089,5.089,0,0,0-4.485-2.355c-2.725,0-4.934,1.806-4.934,4.037H6.058V6.054Z"></path>
+                                </svg>
+                            </a>
+                            <a href="https://www.lib.utk.edu"
+                               class="utk-logo-unit"
+                               tabindex="1">Libraries</a>
+                        </div>
+                    </div>
+                    <div class="utk-header-actions">
+                        <div class="utk-header-actions--item utk-header-actions--home"><a
+                                    href="https://www.lib.utk.edu" tabindex="3">lib.utk.edu</a></div>
+                        <div class="utk-header-actions--item utk-header-actions--search">
+                            <a href="https://www.lib.utk.edu/search"
+                               class="utk-search-trigger utk-search-expand"
+                               tabindex="4">
+                                <span class="icon-search"></span>
+                                <em>Search</em>
+                            </a>
+                        </div>
+                        <div class="utk-header-actions--item utk-header-actions--resources">
+                            <a href="https://www.lib.utk.edu"
+                               class="utk-menu-trigger utk-header-expand"
+                               tabindex="5">
+                                <span class="icon-menu"></span>
+                                <em>Home</em>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="utk-header-super">
+                <div class="container">
+                    <ul class="utk-header-super--menu">
+                        <li><a href="https://www.lib.utk.edu/hours/" tabindex="2">Hours</a></li>
+                        <li><a href="https://libguides.utk.edu/databases" tabindex="2">Articles &amp;
+                                Databases</a></li>
+                        <li><a href="https://www.lib.utk.edu/about/" tabindex="2">A-Z</a></li>
+                        <li><a href="https://www.lib.utk.edu/about/" tabindex="2">About</a></li>
+                        <li><a href="https://www.lib.utk.edu/about/" tabindex="2">Give</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    </div>
+</noscript>
