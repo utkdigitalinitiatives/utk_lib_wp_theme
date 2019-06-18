a6910b540537eb9f8e4d3b1f11ec881e9a8e2d5b
Mat Jordan <mat@utk.edu>
2019-06-18 14:16:03 -0400

    Refine search for google custom search results.

--- resources/views/partials/components/google-custom-search.blade.php
+++ resources/views/partials/components/google-custom-search.blade.php
@@ -4,29 +4,38 @@
     ];
 @endphp
 @if (is_front_page() && in_array(get_current_blog_id(), $allowedSites))
-    <form class="utk-gcs @if(isset($_GET['gcs'])) utk-gcs-active @endif">
-        <div class="utk-gcs--bar">
-            <input name="gcs"
-                   type="text"
-                   placeholder="Find hours, services, and more..."
-                   value="@php echo $_GET['gcs'] @endphp"
-                   class="utk-gcs--search" />
-            @if(isset($_GET['gcs']))
-                <input type="submit"
-                       value="Submit"
-                       class="utk-gcs--submit" />
-            @endif
+    <div class="utk-gcs-wrap @if(isset($_GET['gcs'])) utk-gcs-active @endif">
+        <div class="utk-gcs-results">
+            <form class="utk-gcs">
+                <div class="utk-gcs--bar">
+                    <input name="gcs"
+                           type="text"
+                           placeholder="Find hours, services, and more..."
+                           value="@php echo $_GET['gcs'] @endphp"
+                           class="utk-gcs--search" />
+                    @if(isset($_GET['gcs']))
+                        <input type="submit"
+                               value="Submit"
+                               class="utk-gcs--submit" />
+                    @endif
+                </div>
+                @if(!isset($_GET['gcs']))
+                    <div class="utk-gcs--buttons">
+                        <input type="submit"
+                               value="Search lib.utk.edu"
+                               class="utk-gcs--submit" />
+                        <input type="submit"
+                               value="Switch to OneSearch"
+                               class="utk-gcs--submit" />
+                    </div>
+                @endif
+            </form>
+            <gcse:searchresults-only></gcse:searchresults-only>
         </div>
-        @if(!isset($_GET['gcs']))
-            <div class="utk-gcs--buttons">
-                <input type="submit"
-                       value="Search lib.utk.edu"
-                       class="utk-gcs--submit" />
-                <input type="submit"
-                       value="Switch to OneSearch"
-                       class="utk-gcs--submit" />
-            </div>
+        @if(isset($_GET['gcs']))
+        <div class="utk-gcs-aside">
+            @php // @todo: build side promotions @endphp
+        </div>
         @endif
-    </form>
-    <gcse:searchresults-only></gcse:searchresults-only>
+    </div>
 @endif
