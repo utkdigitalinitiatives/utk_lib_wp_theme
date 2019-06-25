7a20615a10d13e342e325df16196c644ab89b35c
Mat Jordan <mat@utk.edu>
2019-06-25 13:52:57 -0400

    Update home, shifting hours above panel for xs/sm.

--- resources/views/partials/sections/front.blade.php
+++ resources/views/partials/sections/front.blade.php
@@ -2,6 +2,7 @@
 {{--rel="stylesheet"--}}
 {{--href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"--}}
 {{--/>--}}
+<div id="utk-lib-home-hours"></div>
 <div id="utk-panel"></div>
 <section class="utk-home" id="utk-lib-home"></section>
 <section class="utk-section section-callout section-callout--image-text">
