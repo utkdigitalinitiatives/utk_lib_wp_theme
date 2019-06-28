a7a715bdceee7bf67e3c00d29e4a1d9fe604054c
Mat Jordan <mat@utk.edu>
2019-06-28 11:54:31 -0400

    Enqueue local versions of libraries used by react components.

--- app/setup.php
+++ app/setup.php
@@ -12,6 +12,12 @@ use Roots\Sage\Template\BladeProvider;
  * Theme assets
  */
 add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_script('wp/polyfill', '/wp-includes/js/dist/vendor/wp-polyfill.min.js', [], null, true);
+    wp_enqueue_script('wp/react', '/wp-includes/js/dist/vendor/react.min.js', [], null, true);
+    wp_enqueue_script('wp/reactdom', '/wp-includes/js/dist/vendor/react-dom.min.js', [], null, true);
+    wp_enqueue_script('wp/lodash', '/wp-includes/js/dist/vendor/lodash.min.js', [], null, true);
+
+    wp_enqueue_script('ut/header.js', asset_path('scripts/header.js'), [], null, true);
 
     wp_enqueue_style('ut/main.css', asset_path('styles/main.css'), false, null);
 
