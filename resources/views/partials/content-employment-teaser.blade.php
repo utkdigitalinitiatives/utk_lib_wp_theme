37bb0c784ef9e0bdb9b4318b7c23a20e3e265a07
Mat Jordan <mat@utk.edu>
2019-07-11 17:21:35 -0400

    Add new flag to employment posts in past month.

--- resources/views/partials/content-employment-teaser.blade.php
+++ resources/views/partials/content-employment-teaser.blade.php
@@ -4,33 +4,24 @@ $size = 'post-thumbnail';
 $thumbnail  = get_the_post_thumbnail_url($post->ID,$size);
 $dateline   = date('F j, Y', strtotime($post->post_date));
 
-
-
 $category_id        = get_field('employment_category');
 $category           = get_term($category_id);
-$pay_grade          = get_field('employment_pay_grade');
-$salary             = get_field('employment_salary');
-$hours              = get_field('employment_hours');
-$location           = get_field('employment_location');
-$salary             = get_field('employment_salary');
-$salary             = get_field('employment_salary');
 $description        = get_field('employment_description');
-$responsibilities   = get_field('employment_responsibilities');
-$qualifications_r   = get_field('employment_qualifications_required');
-$qualifications_p   = get_field('employment_qualifications_preferred');
-$environment        = get_field('employment_environment');
-$benefits           = get_field('employment_benefits');
-$instructions       = get_field('employment_instructions');
-
-$appointment_rank   = get_field('employment_appointment_rank');
 $department_id      = get_field('employment_department');
 $department         = get_term($department_id);
-$reports_tp         = get_field('employment_reports');
-$available          = get_field('employment_available');
 
+// check this...
 $application_url    = get_field('employment_application_url');
 
 $continue = '...';
+
+
+// @todo check this
+$string =  strtotime($dateline);
+$now =  time();
+if ($now - (604800 * 4) < $string) {
+    print 'new';
+}
 
 @endphp
 
@@ -44,8 +35,8 @@ $continue = '...';
         <main class="article--employment-teaser--main">
             <div class="article--employment-teaser--main--description">
                 <p>@php echo wp_trim_words($description, '30', $continue) @endphp</p>
-                <a href="#" class="btn btn-secondary">Read More</a>
-                <a href="#" class="btn btn-secondary btn-outline">Apply</a>
+                <a href="{{get_the_permalink()}}" class="btn btn-secondary">Read More</a>
+                <a href="{{$application_url}}" class="btn btn-secondary btn-outline">Apply</a>
             </div>
             <div class="article--meta article--employment-teaser--main--meta">
                 <span><strong>Posted</strong> @php echo $dateline @endphp</span>
