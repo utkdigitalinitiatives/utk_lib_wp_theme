9ed4f1143fc170c48f787bcf6f815680c0242abc
Mat Jordan <mat@utk.edu>
2019-07-14 20:23:55 -0400

    Cleanup employment post formatting.

--- resources/views/partials/content-employment.blade.php
+++ resources/views/partials/content-employment.blade.php
@@ -34,66 +34,10 @@
 
         $application_url    = get_field('employment_application_url');
 
-        /*
-         * functionality for required documents
-         */
-
-        $required_docs      = get_field('employment_required');
-
-        if ($required_docs) {
-            $required_docs_gf = implode($required_docs, ',');
-            $gf_shortcode = '[gravityform id=4 ajax=true field_values="options=' . $required_docs_gf . '"]';
-            $next = '?apply=1&step=2';
-        } else {
-            $next = $application_url;
-        }
-
-        /*
-         * get current step
-         */
-
-        if (isset($_REQUEST['step'])) {
-            $step = sanitize_text_field($_REQUEST['step']);
-        } else {
-            $step = 1;
-        }
-
-        /*
-         * get current step
-         */
-
-        if (isset($_REQUEST['step'])) {
-            $step = sanitize_text_field($_REQUEST['step']);
-        } else {
-            $step = 1;
-        }
 
         ?>
 
-        <?php if (!empty($required_docs)) : ?>
-        <div class="employment-steps">
-
-            <?php if ($step == 1) { ?>
-            <div class="step--one active">Description</div>
-            <div class="step--two">Submit Documents</div>
-            <div class="step--three final">Complete Application</div>
-            <?php } ?>
-            <?php if ($step == 2) { ?>
-            <div class="step--one complete"><span class="icon-ok"></span>Description</div>
-            <div class="step--two active">Submit Documents</div>
-            <div class="step--three">Complete Application</div>
-            <?php } ?>
-
-        </div>
-        <?php else : ?>
-
-        <?php endif; ?>
-
-        <?php if ($step == 1) : ?>
-
-        <?php if ($category->name != 'Faculty') : ?>
-        <a class="btn btn-default" href="<?php echo $next; ?>">Apply for Position</a>
-        <?php endif; ?>
+        <a class="btn btn-default" href="<?php echo $apply; ?>">Apply for Position</a>
 
         <div class="employment-options employment-options-flex">
             <?php if (!empty($appointment_rank)) : ?>
@@ -152,75 +96,53 @@
         </div>
 
         <?php if (!empty($hours)) : ?>
-        <h2>Hours</h2>
-        <?php echo $hours; ?>
+            <h2>Hours</h2>
+            <?php echo $hours; ?>
         <?php endif; ?>
 
         <?php if (!empty($location)) : ?>
-        <h2>Location</h2>
-        <?php echo $location; ?>
+            <h2>Location</h2>
+            <?php echo $location; ?>
         <?php endif; ?>
 
         <?php if (!empty($description)) : ?>
-        <h2>Description</h2>
-        <?php echo $description; ?>
+            <h2>Description</h2>
+            <?php echo $description; ?>
         <?php endif; ?>
 
         <?php if (!empty($responsibilities)) : ?>
-        <h2>Responsibilities &amp; Duties</h2>
-        <?php echo $responsibilities; ?>
+            <h2>Responsibilities &amp; Duties</h2>
+            <?php echo $responsibilities; ?>
         <?php endif; ?>
 
         <?php if (!empty($qualifications_r)) : ?>
-        <h2>Required Qualifications</h2>
-        <?php echo $qualifications_r; ?>
+            <h2>Required Qualifications</h2>
+            <?php echo $qualifications_r; ?>
         <?php endif; ?>
 
         <?php if (!empty($qualifications_p)) : ?>
-        <h2>Preferred Qualifications</h2>
-        <?php echo $qualifications_p; ?>
+            <h2>Preferred Qualifications</h2>
+            <?php echo $qualifications_p; ?>
         <?php endif; ?>
 
         <?php if (!empty($environment)) : ?>
-        <h2>Environment</h2>
-        <?php echo $environment; ?>
+            <h2>Environment</h2>
+            <?php echo $environment; ?>
         <?php endif; ?>
 
         <?php if (!empty($benefits)) : ?>
-        <h2>Benefits</h2>
-        <?php echo $benefits; ?>
+            <h2>Benefits</h2>
+            <?php echo $benefits; ?>
         <?php endif; ?>
 
         <?php if (!empty($instructions)) : ?>
-        <h2>Application Procedures</h2>
-        <?php echo $instructions; ?>
+            <h2>Application Procedures</h2>
+            <?php echo $instructions; ?>
         <?php endif; ?>
 
-        <?php $eeo = get_option('options_employment_options_statement'); ?>
         <div class="employment-eeo">
-            <?php print wpautop($eeo); ?>
+            <?php print wpautop(get_option('options_employment_options_statement')); ?>
         </div>
-
-        <?php if ($category->name != 'Faculty') : ?>
-        <a id="employment-footer-apply" class="btn btn-default" href="<?php echo $next; ?>">Apply for Position</a>
-        <?php endif; ?>
-
-        <?php elseif ($step == 2) : ?>
-
-        <?php echo do_shortcode($gf_shortcode); ?>
-
-        <?php if (in_array($category->slug, ['sla'])) : ?>
-        <div class="employment-sla-directions">
-            <?php echo get_post_content_by_id(63); ?>
-        </div>
-        <?php endif; ?>
-
-        <div id="employment-back-button">
-            <a class="btn btn-outline" href="?apply=0&step=<?php echo $step - 1; ?>">Previous</a>
-        </div>
-
-        <?php endif; ?>
-
 
     </div>
 </div>
