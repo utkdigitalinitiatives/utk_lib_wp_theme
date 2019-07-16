b67bc0dcb96cf6d64eaf45032a1dd77b58828d93
Mat Jordan <mat@utk.edu>
2019-07-16 13:51:54 -0400

    Update h1 location in templates.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,6 @@
+
 <header id="utk-lib-header"
+        data-page-title="@php echo get_the_title(); @endphp"
         data-url="@php echo network_site_url(); @endphp">
     @include('partials.header-noscript')
 </header>
