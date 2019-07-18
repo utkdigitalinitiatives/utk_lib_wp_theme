2f1230ff819360426fec91dab1391267b01cd25c
Mat Jordan <mat@utk.edu>
2019-07-18 15:04:25 -0400

    Add information controller.

---
+++ app/Controllers/Information.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+use Sober\Controller\Controller;
+
+class Information extends Controller
+{
+
+    public function getAudienceMeta () {
+        return;
+    }
+
+    public function getInformationList () {
+        return;
+    }
+
+}
