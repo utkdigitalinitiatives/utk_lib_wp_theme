c3973dadf3b753bc7cf42505b2366f6c080c60bc
Mat Jordan <mat@utk.edu>
2019-07-19 17:17:30 -0400

    Update archive for blog style.

--- resources/views/archive.blade.php
+++ resources/views/archive.blade.php
@@ -7,7 +7,7 @@
 
 @section('content')
   @include('partials.page-header')
-  <div class="page-body page-body--archive @if($postStyleClass)page-body-archive-{{$postStyleClass}}@endif">
+  <div class="page-body page-body--archive @if($postStyleClass)page-body--archive-{{$postStyleClass}}@endif">
 
     @if('Blog' === $postStyle)
       @include('partials.sections.main-archive-blog')
