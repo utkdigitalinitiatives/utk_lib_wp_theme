0a87e52021fb4ff149fa26e568f970380d39d1ba
Mat Jordan <mat@utk.edu>
2019-07-19 09:48:58 -0400

    Include basic and functional flat noscripts.

---
+++ resources/views/partials/noscript/noscript-lib-home-panel.blade.php
@@ -0,0 +1,125 @@
+<div class="utk-panel-wrap utk-panel-focus-search">
+    <div class="utk-panel-underlay">
+        <div class="utk-panel-overlay--fade"></div>
+        <div class="utk-panel-underlay--fade-horz"></div>
+        <div class="utk-panel-underlay--fade-vert"></div>
+    </div>
+    <div class="container utk-panel-container">
+        <div class="utk-panel">
+            <div class="utk-panel--search">
+                <div class="utk-search">
+                    <div class="utk-search-tabs">
+                        <div class="ui text menu"><a class="active item">One Search</a><a class="item">Our
+                                Website</a></div>
+                        <div class="ui segment active tab">
+                            <form method="post" action="https://www.lib.utk.edu/search/submit?go=1"
+                                  class="ui form">
+                                <div class="ui input"><input name="method" type="hidden" value="onesearch">
+                                </div>
+                                <div class="ui action input"><input name="value"
+                                                                    placeholder="Search for books, articles, and more..."
+                                                                    aria-label="Search for books, articles, and more..."
+                                                                    type="text">
+                                    <div role="listbox" aria-expanded="false"
+                                         class="ui compact selection dropdown icon-down-open" tabindex="0">
+                                        <div class="text" role="alert" aria-live="polite">One Search</div>
+                                        <div class="menu transition">
+                                            <div role="option" aria-checked="true" aria-selected="true"
+                                                 class="active selected item" style="pointer-events: all;">
+                                                <span class="text">One Search</span></div>
+                                            <div role="option" aria-checked="false" aria-selected="false"
+                                                 class="item" style="pointer-events: all;"><span
+                                                        class="text">UT Collections</span></div>
+                                            <div role="option" aria-checked="false" aria-selected="false"
+                                                 class="item" style="pointer-events: all;"><span
+                                                        class="text">Course Reserves</span></div>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="ui button">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="utk-panel--research-tools">
+                <div class="utk-research-tools"><h3>Research Tools</h3>
+                    <div role="listbox" aria-expanded="false"
+                         class="ui button floating labeled dropdown icon" tabindex="0">
+                        <div class="text" role="alert" aria-live="polite">Browse Databases and Guides</div>
+                        <i aria-hidden="true" class="icon-down-open icon"></i>
+                        <div class="menu transition">
+                            <div class="ui input search"><input placeholder="Filter by Subject Name"
+                                                                type="text"></div>
+                            <div class="scrolling menu transition">
+                                <div role="option" class="item"><span class="text">Advertising &amp; Public Relations</span>
+                                </div>
+                                <div role="option" class="item"><span class="text">African &amp; African American Studies</span>
+                                </div>
+                                <div role="option" class="item"><span class="text">Agriculture</span></div>
+                                <div role="option" class="item"><span class="text">Anthropology</span></div>
+                                <div role="option" class="item"><span
+                                            class="text">Architecture &amp; Design</span></div>
+                                <div role="option" class="item"><span class="text">Art</span></div>
+                                <div role="option" class="item"><span
+                                            class="text">Audiology &amp; Speech Pathology</span>
+                                </div>
+                                <div role="option" class="item"><span
+                                            class="text">Biological Sciences</span></div>
+                                <div role="option" class="item"><span class="text">Business</span></div>
+                                <div role="option" class="item"><span class="text">Chemistry</span></div>
+                                <div role="option" class="item"><span class="text">Child &amp; Family Studies</span>
+                                </div>
+                                <div role="option" class="item"><span class="text">Cinema Studies</span>
+                                </div>
+                                <div role="option" class="item"><span class="text">Classics</span></div>
+                                <div role="option" class="item"><span
+                                            class="text">Communication Studies</span></div>
+                                <div role="option" class="item"><span
+                                            class="text">Earth &amp; Planetary Science</span>
+                                </div>
+                                <div role="option" class="item"><span class="text">Education</span></div>
+                                <div role="option" class="item"><span class="text">Engineering</span></div>
+                                <div role="option" class="item"><span class="text">English</span></div>
+                                <div role="option" class="item"><span class="text">Geography</span></div>
+                                <div role="option" class="item"><span class="text">History</span></div>
+                                <div role="option" class="item"><span
+                                            class="text">Information Sciences</span></div>
+                                <div role="option" class="item"><span
+                                            class="text">Journalism &amp; Electronic Media</span>
+                                </div>
+                                <div role="option" class="item"><span class="text">Mathematics</span></div>
+                                <div role="option" class="item"><span class="text">Modern Foreign Languages &amp; Literatures</span>
+                                </div>
+                                <div role="option" class="item"><span class="text">Music</span></div>
+                                <div role="option" class="item"><span class="text">Nursing</span></div>
+                                <div role="option" class="item"><span class="text">Nutrition</span></div>
+                                <div role="option" class="item"><span class="text">Philosophy</span></div>
+                                <div role="option" class="item"><span
+                                            class="text">Physics &amp; Astronomy</span></div>
+                                <div role="option" class="item"><span class="text">Political Science</span>
+                                </div>
+                                <div role="option" class="item"><span class="text">Psychology</span></div>
+                                <div role="option" class="item"><span class="text">Public Health</span>
+                                </div>
+                                <div role="option" class="item"><span class="text">Religious Studies</span>
+                                </div>
+                                <div role="option" class="item"><span class="text">Social Work</span></div>
+                                <div role="option" class="item"><span class="text">Sociology</span></div>
+                                <div role="option" class="item"><span class="text">Sports Studies</span>
+                                </div>
+                                <div role="option" class="item"><span class="text">Theatre</span></div>
+                                <div role="option" class="item"><span
+                                            class="text">Veterinary Medicine</span></div>
+                                <div role="option" class="item"><span class="text">Women, Gender, &amp; Sexuality Studies</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="utk-collapse-research-tools"><span class="icon-left-big"></span> Reset Options</a>
+                </div>
+            </div>
+            <div class="utk-panel--subject-guide"></div>
+        </div>
+    </div>
+</div>
