bbcbc22d51df89e33430f1908465f6c8b09a5686
Mat Jordan <mat@utk.edu>
2019-07-19 16:40:16 -0400

    Update conditionals to check which archive template to use.

--- resources/views/archive.blade.php
+++ resources/views/archive.blade.php
@@ -1,8 +1,24 @@
 @extends('layouts.app')
 
+@php
+  $postStyle = get_site_option('options_site_post_style');
+  $postStyleClass = sanitize_html_class(strtolower($postStyle));
+@endphp
+
 @section('content')
   @include('partials.page-header')
-  <div class="page-body page-body--archive">
-    @include('partials.sections.main-archive')
+  <div class="page-body page-body--archive @if($postStyleClass)page-body-archive-{{$postStyleClass}}@endif">
+
+    @if('Blog' === $postStyle)
+      @include('partials.sections.main-archive-blog')
+
+    @elseif('Standard' === $postStyle)
+      @include('partials.sections.main-archive')
+
+    @else
+      @include('partials.sections.main-archive')
+
+    @endif
+
   </div>
 @endsection
