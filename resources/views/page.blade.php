127507d4ca1707f9613768964c49c5ced333be00
Mat Jordan <mat@utk.edu>
2019-07-29 13:44:57 -0400

    Update main page template for volopedia entity.

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -2,9 +2,19 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-  @include('partials.page-header')
-  <div class="page-body">
-    @include('partials.sections.main')
-  </div>
+  @if(UT_LIBRARIES_ENTITY === 'volopedia')
+    <div class="utk-volopedia--header" style="background-image: url(<?= \App\asset_path('images/volopedia.jpg') ?>);">
+      @include('partials.page-header')
+      @include('volopedia.partials.glossary-nav')
+    </div>
+    <div class="page-body">
+      @include('volopedia.partials.main')
+    </div>
+  @else
+    @include('partials.page-header')
+    <div class="page-body">
+      @include('partials.sections.main')
+    </div>
+  @endif
   @endwhile
 @endsection
