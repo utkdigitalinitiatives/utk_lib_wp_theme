4f47b078c86e3d699ed0ea6778850a5292415eb7
Mat Jordan <mat@utk.edu>
2019-08-19 00:01:18 -0400

    Update location of notice el lower in dom.

--- resources/views/partials/sections/front.blade.php
+++ resources/views/partials/sections/front.blade.php
@@ -1,4 +1,3 @@
-@include('partials.components.notice')
 <div id="utk-lib-home-hours">
     <noscript>
         @include('partials.noscript.noscript-lib-home-hours')
@@ -9,6 +8,7 @@
         @include('partials.noscript.noscript-lib-home-panel')
     </noscript>
 </div>
+@include('partials.components.notice')
 <section class="utk-home" id="utk-lib-home">
     <noscript>
         @include('partials.noscript.noscript-lib-home')
