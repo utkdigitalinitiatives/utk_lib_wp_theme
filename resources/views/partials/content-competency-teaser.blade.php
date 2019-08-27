64171ad807379bab15f27ebec0aad67039b49f19
Mat Jordan <mat@utk.edu>
2019-08-27 10:34:24 -0400

    Appply grid proportion refinement responsively.

--- resources/views/partials/content-competency-teaser.blade.php
+++ resources/views/partials/content-competency-teaser.blade.php
@@ -1,25 +1,48 @@
 @php
 
-$model = wp_get_post_terms(get_the_ID(), 'competency_models', array("fields" => "all"))
+$model = wp_get_post_terms(get_the_ID(), 'competency_models', array("fields" => "all"));
+
+$words = preg_split("/[\s,_-]+/", get_the_title());
+$acronym = "";
+
+$exclude = [
+    '&',
+    'a'
+];
+
+foreach ($words as $w) {
+    if (!in_array($w[0], $exclude)) {
+        $acronym .= $w[0];
+    }
+}
+
+$acronym = strtoupper(substr($acronym, 0, 3))
+
+
 
 @endphp
 
 <article @php post_class() @endphp>
-    <div class="article--context">
-        <a  class="article--grid--image"
-            href="@php the_permalink() @endphp">
-            <figure></figure>
-            <div class="article--grid--image--overlay"></div>
-        </a>
-        <header class="article--header">
-            <h2 class="article--header--title entry-title">
-                <a href="@php the_permalink() @endphp">@php the_title() @endphp</a>
-            </h2>
-            <div class="article--meta article--meta-excerpt">
-                <div class="article--meta article--meta-categories">
-                    @php print $model[0]->name @endphp
-                </div>
+    <a class="article--trigger"
+        href="@php the_permalink() @endphp">
+        <div class="article--context">
+            <div class="article--grid--image"
+                href="@php the_permalink() @endphp">
+                <figure>
+                    <span>{{$acronym}}</span>
+                </figure>
+                <div class="article--grid--image--overlay"></div>
             </div>
-        </header>
-    </div>
+            <header class="article--header">
+                <h2 class="article--header--title entry-title">
+                    @php the_title() @endphp
+                </h2>
+                <div class="article--meta article--meta-excerpt">
+                    <div class="article--meta article--meta-categories">
+                        @php print $model[0]->name @endphp
+                    </div>
+                </div>
+            </header>
+        </div>
+    </a>
 </article>
