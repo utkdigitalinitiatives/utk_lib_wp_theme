9cabbfb1fd048392c2d9b517a150ea90f784eeaf
Mat Jordan <mat@utk.edu>
2019-08-28 15:41:42 -0400

    Fix method name bug on wp standard template.

--- resources/views/partials/content-competency.blade.php
+++ resources/views/partials/content-competency.blade.php
@@ -5,7 +5,7 @@
     $post_id = get_the_ID();
     $post_type = get_post_type();
 
-    $post = Formal::get_formal_post($post_id, $post_type);
+    $post = Formal::getFormalPost($post_id, $post_type);
 
     print $post;
 
