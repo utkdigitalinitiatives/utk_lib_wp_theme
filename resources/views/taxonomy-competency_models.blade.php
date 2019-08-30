b9996d6510a3610cd2463bd37d73ff946f407984
Mat Jordan <mat@utk.edu>
2019-08-30 11:24:22 -0400

    Reference formal archive for comptentency model terms.

---
+++ resources/views/taxonomy-competency_models.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+@include('partials.page-header')
+<div class="page-body page-body-nosidebar page-body--archive">
+    @include('partials.sections.main-formal-archive')
+</div>
+@endsection
