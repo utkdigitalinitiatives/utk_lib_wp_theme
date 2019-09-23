a3b4e94afe1859232cbc62e4372644ad3ccb2219
Mat Jordan <mat@utk.edu>
2019-09-23 14:46:22 -0400

    Add permalink to full content.

--- resources/views/partials/content-space-modal.blade.php
+++ resources/views/partials/content-space-modal.blade.php
@@ -27,29 +27,29 @@
 @endphp
 <article @php post_class('space', $id) @endphp>
     <div class="utk-space--content">
-        <header>
-            <h3>@php echo $data['title']; @endphp</h3>
-        </header>
-        <div class="utk-space-actions">
-            <div class="utk-date-picker">
-                <a  class="utk-date-picker-select" href="#">Monday 9/23/19 <span class="icon-down-open"></span></a>
-                <div class="utk-space--time">
-                    <span class="utk-space--time--indicator"></span>
-                    <span class="utk-space--time--span">8am - 6pm</span>
+        <div class="utk-space--content--wrap">
+            <header>
+                <h3>@php echo $data['title']; @endphp</h3>
+            </header>
+            <div class="utk-space-actions">
+                <div class="utk-date-picker">
+                    <a  class="utk-date-picker-select" href="#">Monday 9/23/19 <span class="icon-down-open"></span></a>
+                    <div class="utk-space--time">
+                        <span class="utk-space--time--indicator"></span>
+                        <span class="utk-space--time--span">8am - 6pm</span>
+                    </div>
                 </div>
             </div>
-        </div>
-        <span class="utk-modal-separator"></span>
-        <div class="utk-modal-meta">
-            <div class="utk-modal-meta--item">
-                <span class="utk-modal-meta--item--label">Location</span>
-                <span class="utk-modal-meta--item--value">@php echo Space::getSpaceLocations($id, true); @endphp</span>
+            <span class="utk-modal-separator"></span>
+            <div class="utk-modal-meta">
+                <div class="utk-modal-meta--item">
+                    <span class="utk-modal-meta--item--label">Location</span>
+                    <span class="utk-modal-meta--item--value">@php echo Space::getSpaceLocations($id, true); @endphp</span>
+                </div>
+                @php echo Space::getSpaceFloor($floor); @endphp
+                @php echo Space::getSpaceRooms($rooms); @endphp
             </div>
-            @php echo Space::getSpaceFloor($floor); @endphp
-            @php echo Space::getSpaceRooms($rooms); @endphp
-        </div>
-        <span class="utk-modal-separator"></span>
-        <div class="utk-space--content--wrap">
+            <span class="utk-modal-separator"></span>
             <div class="utk-modal-meta utk-modal-meta-list">
                 <div class="utk-modal-meta--item">
                     <span class="utk-modal-meta--item--label">
@@ -68,14 +68,13 @@
         </div>
     </div>
     <div class="utk-space--media">
-        <div class="utk-space--media--slider"
-             href="@php the_permalink() @endphp">
+        <div class="utk-space--media--slider">
             @include('partials.components.image')
             <div class="utk-space--media--slider--overlay">
                 <a href="#" class="btn btn-primary">
                     Reserve
                 </a>
-                <a href="#"  class="btn btn-with-icon">
+                <a href="@php the_permalink($id) @endphp"  class="btn btn-with-icon">
                     Read More
                     <span class="icon-right-open"></span>
                 </a>
