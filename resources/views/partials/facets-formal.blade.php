6b9e481c58153503bc9dbda3fca11f3a6f9baff0
Mat Jordan <mat@utk.edu>
2019-10-01 14:16:57 -0400

    Restore filter and reset buttons.

--- resources/views/partials/facets-formal.blade.php
+++ resources/views/partials/facets-formal.blade.php
@@ -26,6 +26,14 @@ $facets = 'formal_facets';
                     <em>{{$type_object->labels->name}}</em>
                     <span class="utk-facets--label-data utk-facets--label-string"></span>
                 </div>
+                <div class="utk-facets--filter">
+                    <button class="utk-facets--button utk-facets--reset">
+                        Reset
+                    </button>
+                    <button class="utk-facets--button utk-facets--toggle">
+                        Filter
+                    </button>
+                </div>
             </div>
             <div class="utk-facets--modal">
                 <button class="utk-facets--button utk-facets--close"
