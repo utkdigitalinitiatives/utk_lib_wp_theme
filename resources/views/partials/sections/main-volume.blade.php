10df5a60173f22024ea4cf147bfe0bc27342e0b6
Mat Jordan <mat@utk.edu>
2019-10-24 16:48:29 -0400

    Build floating title.

--- resources/views/partials/sections/main-volume.blade.php
+++ resources/views/partials/sections/main-volume.blade.php
@@ -1,5 +1,12 @@
 <div id="detach-sticky-top"></div>
 <div class="utk-formal--header--background">
+    <div class="utk-volume--title">
+        <div class="container">
+            <span class="utk-volume--title--series">Boundless Series</span>
+            <h1>@php echo get_the_title() @endphp</h1>
+            <span class="utk-volume--title--intro">RB Morries donec venenatis risus at ligula vestibulum malesuada. Mauris vitae orci. </span>
+        </div>
+    </div>
     @php
         print get_the_post_thumbnail(null, 'hero_image');
     @endphp
