003c28214808f94716fb36b93e4d10956bbe6387
Mat Jordan <mat@utk.edu>
2019-10-25 16:52:54 -0400

    Add link to posts in volume teasers.

--- resources/views/volumes/content-volume-teaser.blade.php
+++ resources/views/volumes/content-volume-teaser.blade.php
@@ -1,5 +1,5 @@
 <div class="page-body--content--inner">
-    @php echo get_the_title() @endphp
+    <a href="{{get_the_permalink()}}">@php echo get_the_title() @endphp</a>
     <div class="page-body--content--body">
         @php the_content() @endphp
     </div>
