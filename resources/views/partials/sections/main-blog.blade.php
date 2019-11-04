7a6feab2dd74fc455d383f21c782c451022dda0f
Mat Jordan <mat@utk.edu>
2019-11-04 10:56:41 -0500

    Adjust contrast on overlay.

--- resources/views/partials/sections/main-blog.blade.php
+++ resources/views/partials/sections/main-blog.blade.php
@@ -2,7 +2,6 @@
 @include('volumes.components.single-masthead')
 <div class="container page-body--container">
     <div class="page-body--flex">
-        <span class="page-body--tag">Features</span>
         <main class="page-body--content page-body--content-full">
             @include('partials.content-post-blog')
         </main>
