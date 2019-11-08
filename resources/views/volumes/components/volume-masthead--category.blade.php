fc65abb5063e6b56a0c99c22b898e1cc5e7d53b1
Mat Jordan <mat@utk.edu>
2019-11-08 15:05:26 -0500

    Supplant hardcoded fields for taxonomy term template.

--- resources/views/volumes/components/volume-masthead--category.blade.php
+++ resources/views/volumes/components/volume-masthead--category.blade.php
@@ -1,10 +1,16 @@
+@php
+    $id = get_queried_object()->term_id;
+    $term = get_term($id);
+@endphp
 <div class="utk-formal--header--background">
     <div class="utk-volume--title">
         <div class="container">
             <div class="utk-volume--title--inner">
-                <h1>Boundless</h1>
-                <h2>Artists in the Archives</h2>
-                <span class="utk-volume--title--intro">Fifteen or less words describing the set of volumes under this category.</span>
+                <h1>@php print $term->name; @endphp</h1>
+                @if (get_field('taxonomy_subtitle', $term->taxonomy . '_' . $id))
+                    <h2>@php echo get_field('taxonomy_subtitle', $term->taxonomy . '_' . $id) @endphp</h2>
+                @endif
+                <span class="utk-volume--title--intro">@php print $term->description; @endphp</span>
             </div>
         </div>
     </div>
