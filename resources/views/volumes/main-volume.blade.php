bdf9793fc328ee407476984be50fce7c31b6c1a5
Mat Jordan <mat@utk.edu>
2019-11-08 14:25:38 -0500

    mend

--- resources/views/volumes/main-volume.blade.php
+++ resources/views/volumes/main-volume.blade.php
@@ -6,7 +6,7 @@
             @include('volumes.content-volume')
         </main>
         <aside class="page-body--aside page-body--aside-right">
-            @include('volumes.partials.related-links')
+            @include('volumes.partials.related-content')
             <iframe style="border: 0; height: 470px;"
                     src="https://bandcamp.com/EmbeddedPlayer/album=1216554894/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/track=3620167624/transparent=true/"
                     seamless><a href="http://utklibraries.bandcamp.com/album/boundless">Boundless by R.B. Morris</a>
