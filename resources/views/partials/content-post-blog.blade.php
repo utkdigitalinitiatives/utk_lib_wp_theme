559bcd38c7d5e8f2d4b1bdf78a64e336d4faffba
Mat Jordan <mat@utk.edu>
2019-11-11 15:44:31 -0500

    Add custom author capability to feature template.

--- resources/views/partials/content-post-blog.blade.php
+++ resources/views/partials/content-post-blog.blade.php
@@ -9,8 +9,14 @@ Namespace App\Controllers;
             $dateline2   = $post->post_modified;
             $dateline   = date('F j, Y', strtotime($post->post_date));
             $avatar     = get_avatar_data(get_post_meta($post->post_author));
-            $author     = get_the_author_meta('display_name', $post->post_author);
-            $estimate   = App::estimateReadTime($post->post_content)
+            $estimate   = App::estimateReadTime($post->post_content);
+
+            if (get_field('feature_custom_author')) :
+                $author = get_field('feature_custom_author');
+            else :
+                $author = get_the_author_meta('display_name', $post->post_author);
+            endif;
+
         @endphp
         <span class="utk-heading-1" role="heading" aria-level="1">@php echo get_the_title(); @endphp</span>
         <div class="page-body-blog--article">
