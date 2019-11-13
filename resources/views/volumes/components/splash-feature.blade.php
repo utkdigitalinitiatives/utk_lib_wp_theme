f65fe707ad99e79bcc5c85f3393b57fb41e461f0
Mat Jordan <mat@utk.edu>
2019-11-13 14:31:38 -0500

    Style homepage feature links.

--- resources/views/volumes/components/splash-feature.blade.php
+++ resources/views/volumes/components/splash-feature.blade.php
@@ -1,19 +1,37 @@
 @php
     $primary = Volumes::getFeaturedFeature();
-    $secondary = Volumes::getQuickPosts('feature', $primary, 4);
+    $secondary = Volumes::getQuickPosts('feature', $primary, 5);
 @endphp
 <div class="utk-splash---feature">
     <div class="utk-splash---feature--primary">
-        <a href="@php echo get_the_permalink($primary) @endphp">
+        <a class="feature-post"
+           href="@php echo get_the_permalink($primary) @endphp">
             @php echo get_the_post_thumbnail($primary, 'callout_image') @endphp
             <h3>@php echo get_the_title($primary); @endphp</h3>
+            <span class="utk-splash---feature--meta">
+                        @if (get_field('feature_custom_author', $id))
+                    @php echo get_field('feature_custom_author', $id) @endphp
+                @else
+                    @php echo get_the_author_meta('display_name', $post->post_author) @endphp
+                @endif
+            </span>
         </a>
     </div>
     <div class="utk-splash---feature--secondary">
         @foreach($secondary as $id)
-            <a href="@php echo get_the_permalink($id) @endphp">
+            <a class="feature-post"
+               href="@php echo get_the_permalink($id) @endphp">
                 @php echo get_the_post_thumbnail($id, 'callout_image') @endphp
-                <h3>@php echo get_the_title($id); @endphp</h3>
+                <div>
+                    <h3>@php echo get_the_title($id); @endphp</h3>
+                    <span class="utk-splash---feature--meta">
+                        @if (get_field('feature_custom_author', $id))
+                            @php echo get_field('feature_custom_author', $id) @endphp
+                        @else
+                            @php echo get_the_author_meta('display_name', $post->post_author) @endphp
+                        @endif
+                    </span>
+                </div>
             </a>
         @endforeach
     </div>
