e1dbecac3f7ec32c814d67403265ef622387ae22
Mat Jordan <mat@utk.edu>
2019-11-13 14:02:42 -0500

    Style desktop homepage for volumes.

--- resources/views/volumes/components/splash-feature.blade.php
+++ resources/views/volumes/components/splash-feature.blade.php
@@ -1,19 +1,19 @@
 @php
     $primary = Volumes::getFeaturedFeature();
-    $secondary = Volumes::getQuickPosts('feature', $primary, 3);
+    $secondary = Volumes::getQuickPosts('feature', $primary, 4);
 @endphp
 <div class="utk-splash---feature">
     <div class="utk-splash---feature--primary">
         <a href="@php echo get_the_permalink($primary) @endphp">
-            <h2>@php echo get_the_title($primary); @endphp</h2>
             @php echo get_the_post_thumbnail($primary, 'callout_image') @endphp
+            <h3>@php echo get_the_title($primary); @endphp</h3>
         </a>
     </div>
     <div class="utk-splash---feature--secondary">
         @foreach($secondary as $id)
             <a href="@php echo get_the_permalink($id) @endphp">
-                <h2>@php echo get_the_title($id); @endphp</h2>
                 @php echo get_the_post_thumbnail($id, 'callout_image') @endphp
+                <h3>@php echo get_the_title($id); @endphp</h3>
             </a>
         @endforeach
     </div>
