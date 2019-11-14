ff32a60e4e45c017fe6f77e5a9a50b31e3aa1807
Mat Jordan <mat@utk.edu>
2019-11-14 09:43:40 -0500

    Fix fadeHeader() bug on ready #2.

--- resources/assets/scripts/components/Volumes.js
+++ resources/assets/scripts/components/Volumes.js
@@ -33,29 +33,29 @@ export default class Volumes {
                     }
                 });
             });
-            //
-            // $(document).ready(function() {
-            //     let trigger = document.getElementById('utk-trigger-header');
-            //     let triggerBounds = Header.getBounds(trigger);
-            //
-            //     $('.utk-entity-volumes .page-header--title-wrap').css({
-            //         opacity: function() {
-            //             return triggerBounds.top / 200;
-            //         }
-            //     });
-            //
-            //     $('.utk-entity-volumes .page-header-truncate .page-header--float .page-header--float--optional-overlay').css({
-            //         opacity: function() {
-            //             return triggerBounds.top / 200;
-            //         }
-            //     });
-            //
-            //     $('.utk-entity-volumes #menu-subsite-dropdown-wrap').css({
-            //         opacity: function() {
-            //             return triggerBounds.top / 200;
-            //         }
-            //     });
-            // });
+
+            $(document).ready(function() {
+                let trigger = document.getElementById('utk-trigger-header');
+                let triggerBounds = Header.getBounds(trigger);
+
+                $('.utk-entity-volumes .page-header--title-wrap').css({
+                    opacity: function() {
+                        return triggerBounds.top / 200;
+                    }
+                });
+
+                $('.utk-entity-volumes .page-header-truncate .page-header--float .page-header--float--optional-overlay').css({
+                    opacity: function() {
+                        return triggerBounds.top / 200;
+                    }
+                });
+
+                $('.utk-entity-volumes #menu-subsite-dropdown-wrap').css({
+                    opacity: function() {
+                        return triggerBounds.top / 200;
+                    }
+                });
+            });
         })(jQuery, console.log);
     }
 }
