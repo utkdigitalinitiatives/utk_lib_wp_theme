3eb7dbbd1c201debfdff5e3774a96197e3a98f18
Mat Jordan <mat@utk.edu>
2019-12-05 13:44:04 -0500

    Style space single header actions.

--- resources/views/partials/content-space.blade.php
+++ resources/views/partials/content-space.blade.php
@@ -1,17 +1,33 @@
 @php
 
+    Namespace UTKLibrary\Library\Models;
+
     $data['id'] = get_the_ID();
-    $data['fields'] = get_fields()
+    $data['fields'] = get_fields();
+
+    $reserve = $data['fields']['space_reserve'];
+    $reserve_url = $data['fields']['space_reseve_url'];
+
+    $space_daypicker = 'show';
+    $space_hours = 'inherit';
+    $space_lid = Space::getLocationLID($data['id']);
+    $space_message = null;
 
 @endphp
 <article @php post_class() @endphp>
     <div class="utk-space--sheet">
         <div class="utk-space--details">
-            <div class="page-body--content--inner--interactions">
-                <a href="@php echo get_post_type_archive_link('space') @endphp"
-                   class="utk-space--back btn btn-primary">Back to Spaces</a>
+            <div class="utk-space--details--interactions">
+                @include('partials.components.space-hours')
+                <div class="utk-space--details--interactions--steps">
+                    <a href="@php echo get_post_type_archive_link('space') @endphp"
+                       class="utk-space--back btn btn-sm btn-secondary btn-inverse btn-outline">Back to Spaces</a>
+                    @if($reserve === 'Yes')
+                        <a href="{{$reserve_url}}"
+                           class="btn btn-sm btn-secondary btn-inverse btn-outline">Reserve Space <span class="icon-right-big"></span></a>
+                    @endif
+                </div>
             </div>
-            <div>image gallery</div>
             <div>info</div>
             <div>description</div>
         </div>
