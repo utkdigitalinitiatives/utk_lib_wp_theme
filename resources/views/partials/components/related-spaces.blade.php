c6f1934e490c75820a74305bcaced3884ed3ef27
Mat Jordan <mat@utk.edu>
2019-12-09 11:41:15 -0500

    Add related spaces title.

--- resources/views/partials/components/related-spaces.blade.php
+++ resources/views/partials/components/related-spaces.blade.php
@@ -5,11 +5,14 @@
     $related = Space::getRelatedSpaces($data['id'], 4);
 
 @endphp
-<div class="utk-spaces--related">
-    @foreach($related as $space)
-        @php
-            $space_id = $space;
-        @endphp
-        @include('partials.content-space-teaser')
-    @endforeach
+<div class="utk-space--related">
+    <h3>Spaces Similar to @php echo get_the_title($data['id']) @endphp</h3>
+    <div class="utk-space--related--items">
+        @foreach($related as $space)
+            @php
+                $space_id = $space;
+            @endphp
+            @include('partials.content-space-teaser')
+        @endforeach
+    </div>
 </div>
