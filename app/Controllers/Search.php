b7458053dbe01959aed2de0f19edcf17a5f74eb5
Mat Jordan <mat@utk.edu>
2019-12-16 09:42:19 -0500

    Update search endpoint to PrimoVE.

--- app/Controllers/Search.php
+++ app/Controllers/Search.php
@@ -47,7 +47,7 @@ class Search extends Controller
         endif;
 
         // base url for primo
-        $url="https://utk-almaprimo.hosted.exlibrisgroup.com/primo-explore";
+        $url="https://utk.primo.exlibrisgroup.com/discovery";
         $url.="/search?vid=01UTK&institution=01UTK";
         $url.="&facet=rtype,exclude,reviews&facet=rtype,exclude,reference_entrys";
         $url.="&search_scope=" . $scope . '&tab=' . $tab;
