6bc24745fd3ab183f746845b77bbca65362c3aef
Mat Jordan <mat@utk.edu>
2020-01-20 08:44:10 -0500

    Draft mock design for Rising from the Ashes.

---
+++ resources/views/page-story.blade.php
@@ -0,0 +1,23 @@
+@php
+/**
+* Template Name: Story
+* Description: Page with formal heading for sites or content without need for sidebar.
+*
+* @package Sage
+* @subpackage UT Libraries
+*/
+
+Namespace App\Controllers;
+
+@endphp
+
+@extends('layouts.app')
+
+@section('content')
+@while(have_posts()) @php the_post() @endphp
+@include('partials.page-header-truncate')
+<div class="page-body page-body-truncate page-body-nosidebar">
+    @include('partials.sections.main-story')
+</div>
+@endwhile
+@endsection
---
+++ resources/views/partials/sections/main-story.blade.php
@@ -0,0 +1,37 @@
+@php
+
+  Namespace App\Controllers;
+
+  $formal_type = get_field('formal_type', null, false);
+
+  if (strtolower($formal_type) === 'page') :
+      $formal_header = get_field('formal_header', null, false);
+  else :
+      $formal_header = get_the_ID();
+  endif;
+
+  $storyHeader = \App\asset_path('images/gatlingburg-fires-background_2fbdab00.jpg');
+
+@endphp
+<div id="detach-sticky-top"></div>
+{{--@include('partials.components.breadcrumb')--}}
+<div class="page-story">
+  <header class="page-story--header">
+    <div class="page-story--header--title">
+      <h1>Rising from the Ashes</h1>
+      <h2>An Oral History of the 2016 Gatlinburg Fires</h2>
+    </div>
+    <img class="page-story--header--background" src="@php print $storyHeader; @endphp"/>
+  </header>
+  <div class="container page-body--container">
+    <div class="page-body--flex">
+      <aside class="page-body--aside page-body--aside-hidden">
+        @include('partials.sidebar')
+      </aside>
+      <main class="page-body--content page-body--content-formal">
+        @include('partials.content-page--formal')
+      </main>
+    </div>
+    <div id="detach-sticky-bottom"></div>
+  </div>
+</div>
