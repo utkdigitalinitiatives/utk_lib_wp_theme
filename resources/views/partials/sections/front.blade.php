bfa87fdae8c687819e1a9b48380b7c554d03a0df
Mat Jordan <mat@utk.edu>
2020-02-26 15:07:27 -0500

    Fix homepage section ordering...

--- resources/views/partials/sections/front.blade.php
+++ resources/views/partials/sections/front.blade.php
@@ -3,14 +3,14 @@
         @include('partials.noscript.noscript-lib-home-hours')
     </noscript>
 </div>
+<div id="utk-panel">
+    <noscript>
+        @include('partials.noscript.noscript-lib-home-panel')
+    </noscript>
+</div>
 @include('partials.components.notice')
 <section class="utk-home" id="utk-lib-home">
     <noscript>
         @include('partials.noscript.noscript-lib-home')
     </noscript>
 </section>
-<div id="utk-panel">
-  <noscript>
-    @include('partials.noscript.noscript-lib-home-panel')
-  </noscript>
-</div>
