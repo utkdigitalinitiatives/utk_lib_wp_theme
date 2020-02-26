5c98e9a320b0d12c5b55eca119e3f50312de3245
Mat Jordan <mat@utk.edu>
2020-02-26 14:59:38 -0500

    Fix mobile hours menu for nojs.

--- resources/views/partials/noscript/noscript-lib-home-hours.blade.php
+++ resources/views/partials/noscript/noscript-lib-home-hours.blade.php
@@ -1,86 +1,128 @@
 @php
 
-namespace UTKLibrary\Library\App\Extras;
+  namespace UTKLibrary\Library\App\Extras;
 
 @endphp
 <div class="utk-hours">
-    <div class="utk-hours-header"><h3>Libraries &amp; Locations</h3></div>
-    <div class="utk-hours--listing">
-        <ul class="utk-hours--listing--col"
-            aria-label="hodges library and locations inside">
-            <li class="utk-hours--listing--item "><a href="https://lib.utk.edu">
-                    <figure><img
-                                src="/wp-content/themes/utk_lib_wp_theme/resources/assets/react/header/05f11cf6906e716ce2f823daeec0db30.jpg"
-                                alt="John C. Hodges Library"></figure>
-                    <div class="utk-hours--listing--item--meta"><span
-                                class="library-title">Hodges</span><span
-                                class="library-subtitle">Main Library</span><span
-                                class="utk-hours--listing--item--hours">@php(LibCal::the_hours_by_lid(52))</span>
-                    </div>
-                </a>
-                <ul class="utk-hours--listing--item--childmenu"
-                    aria-label="locations within Hodges">
-                    <li class="utk-hours--listing--item utk-hours--listing--item--child">
-                        <a href="https://commons.utk.edu/">
-                            <div class="utk-hours--listing--item--meta"><span
-                                        class="library-title">The Commons</span><span
-                                        class="library-subtitle">Hodges 2nd Floor</span><span
-                                        class="utk-hours--listing--item--hours">@php(LibCal::the_hours_by_lid(52))</span>
-                            </div>
-                        </a></li>
-                    <li class="utk-hours--listing--item utk-hours--listing--item--child">
-                        <a href="https://www.lib.utk.edu/studio">
-                            <div class="utk-hours--listing--item--meta"><span
-                                        class="library-title">The Studio</span><span
-                                        class="library-subtitle">Hodges 235</span><span
-                                        class="utk-hours--listing--item--hours">@php(LibCal::the_hours_by_lid(217))</span>
-                            </div>
-                        </a></li>
-                    <li class="utk-hours--listing--item utk-hours--listing--item--child">
-                        <a href="https://www.lib.utk.edu/special">
-                            <div class="utk-hours--listing--item--meta"><span
-                                        class="library-title">Special Collections</span><span
-                                        class="library-subtitle">Hodges 121</span><span
-                                        class="utk-hours--listing--item--hours">@php(LibCal::the_hours_by_lid(224))</span>
-                            </div>
-                        </a></li>
-                </ul>
-                <a class="utk-hours--listing--item--more"><span
-                            class="icon-plus">3</span> Show More at Hodges</a></li>
+  <div class="utk-hours-teaser"><span class="utk-hours-teaser--title">Find Your Place</span>
+    <div class="utk-hours-teaser--menu"><a href="https://www.lib.utk.edu/hours/">Locations</a><a
+        href="https://www.lib.utk.edu/hours/spaces/">Explore Spaces</a><a
+        href="https://www.lib.utk.edu/hours/maps-directions/">Maps &amp; Directions</a></div>
+    <img src="/wp-content/themes/utk_lib_wp_theme/resources/assets/react/header/01254d541c35d5c76d787260cb118443.jpg">
+  </div>
+  <div class="utk-hours-header"><h3>Hours for February 26, 2020</h3>
+    <div class="utk-hours-header--links"><a href="https://www.lib.utk.edu/hours/" class="utk-resources-toggle--map">More
+        Hours</a></div>
+  </div>
+  <div class="utk-hours--listing">
+    <ul class="utk-hours--listing--col" aria-label="libraries and locations">
+      <li class="utk-hours--listing--item "><a href="https://lib.utk.edu" class="library-open">
+          <figure><img
+              src="/wp-content/themes/utk_lib_wp_theme/resources/assets/react/header/05f11cf6906e716ce2f823daeec0db30.jpg"
+              alt="John C. Hodges Library"></figure>
+        </a>
+        <div class="utk-hours--listing--item--meta"><a href="https://lib.utk.edu" class="library-open"><span
+              class="library-title">Hodges</span><span class="library-subtitle">Main Library</span><span
+              class="utk-hours--listing--item--hours">@php(LibCal::the_hours_by_lid(52))</span></a>
+          <div class="utk-hours--listing--item--meta--links">
+            <div class="utk-hours--listing--item--meta--links--spaces"><a
+                href="https://www.lib.utk.edu/hours/spaces/the-math-place/"><img alt="The Math Place"
+                                                                                 src="https://www.lib.utk.edu/wp-content/blogs.dir/44/files/2020/01/the-math-place-commons-199x123.jpg"></a><a
+                href="https://www.lib.utk.edu/hours/spaces/study-rooms-3rd-floor/"><img alt="Study Rooms - 3rd Floor"
+                                                                                        src="https://www.lib.utk.edu/wp-content/blogs.dir/44/files/2020/01/hodges-study-rooms-199x123.jpg"></a><a
+                href="https://www.lib.utk.edu/hours/spaces/?fwp_location=hodges">View <br>Spaces <span
+                  class="sr-only">at hodges</span></a></div>
+          </div>
+        </div>
+        <ul class="utk-hours--listing--item--childmenu" aria-label="locations within Hodges">
+          <li class="utk-hours--listing--item utk-hours--listing--item--child"><a href="https://commons.utk.edu"
+                                                                                  class="library-open">
+              <div class="utk-hours--listing--item--meta"><span class="library-title">The Commons</span><span
+                  class="library-subtitle">Hodges 2nd Floor</span><span
+                  class="utk-hours--listing--item--hours">@php(LibCal::the_hours_by_lid(52))</span>
+              </div>
+            </a></li>
+          <li class="utk-hours--listing--item utk-hours--listing--item--child"><a href="https://lib.utk.edu"
+                                                                                  class="library-open">
+              <div class="utk-hours--listing--item--meta"><span class="library-title">Graduate Commons</span><span
+                  class="library-subtitle">Hodges 131</span><span
+                  class="utk-hours--listing--item--hours">@php(LibCal::the_hours_by_lid(52))</span></div>
+            </a></li>
+          <li class="utk-hours--listing--item utk-hours--listing--item--child"><a
+              href="https://www.lib.utk.edu/studio" class="library-open">
+              <div class="utk-hours--listing--item--meta"><span class="library-title">The Studio</span><span
+                  class="library-subtitle">Hodges 235</span><span
+                  class="utk-hours--listing--item--hours">@php(LibCal::the_hours_by_lid(217))</span>
+              </div>
+            </a></li>
+          <li class="utk-hours--listing--item utk-hours--listing--item--child"><a
+              href="https://www.lib.utk.edu/special" class="library-open">
+              <div class="utk-hours--listing--item--meta"><span class="library-title">Special Collections</span><span
+                  class="library-subtitle">Hodges 121</span><span
+                  class="utk-hours--listing--item--hours">@php(LibCal::the_hours_by_lid(224))</span></div>
+            </a></li>
         </ul>
-        <ul class="utk-hours--listing--col" aria-label="other libraries on campus">
-            <li class="utk-hours--listing--item "><a href="https://lib.utk.edu/agvet">
-                    <figure><img
-                                src="/wp-content/themes/utk_lib_wp_theme/resources/assets/react/header/0cbe268ec955cd0bcc2ee7d30fb21b7f.jpg"
-                                alt="Pendergrass Agriculture &amp; Veterinary Medicine Library">
-                    </figure>
-                    <div class="utk-hours--listing--item--meta"><span
-                                class="library-title">Pendergrass</span><span
-                                class="library-subtitle">AgVet Library</span><span
-                                class="utk-hours--listing--item--hours">@php(LibCal::the_hours_by_lid(225))</span>
-                    </div>
-                </a></li>
-            <li class="utk-hours--listing--item "><a href="https://lib.utk.edu/music">
-                    <figure><img
-                                src="/wp-content/themes/utk_lib_wp_theme/resources/assets/react/header/d9763b4914218a8fb970d6545997d543.jpg"
-                                alt="George F. DeVine Music Library"></figure>
-                    <div class="utk-hours--listing--item--meta"><span
-                                class="library-title">DeVine</span><span
-                                class="library-subtitle">Music Library</span><span
-                                class="utk-hours--listing--item--hours">@php(LibCal::the_hours_by_lid(226))</span>
-                    </div>
-                </a></li>
-            <li class="utk-hours--listing--item "><a
-                        href="https://lib.utk.edu/request/storage">
-                    <figure><img
-                                src="/wp-content/themes/utk_lib_wp_theme/resources/assets/react/header/dff1a40b70fc66eaf0abdf8f6ce372d2.jpg"
-                                alt="James D. Hoskins Library"></figure>
-                    <div class="utk-hours--listing--item--meta"><span
-                                class="library-title">Hoskins</span><span
-                                class="library-subtitle">Storage &amp; Reading Room</span><span
-                                class="utk-hours--listing--item--hours">@php(LibCal::the_hours_by_lid(227))</span>
-                    </div>
-                </a></li>
-        </ul>
-    </div>
+        <a class="utk-hours--listing--item--more"><span class="icon-plus">4</span> Show More at Hodges</a></li>
+      <li class="utk-hours--listing--item "><a href="https://lib.utk.edu/agvet" class="library-open">
+          <figure><img
+              src="/wp-content/themes/utk_lib_wp_theme/resources/assets/react/header/0cbe268ec955cd0bcc2ee7d30fb21b7f.jpg"
+              alt="Pendergrass Agriculture &amp; Veterinary Medicine Library"></figure>
+        </a>
+        <div class="utk-hours--listing--item--meta"><a href="https://lib.utk.edu/agvet" class="library-open"><span
+              class="library-title">Pendergrass</span><span class="library-subtitle">AgVet Library</span><span
+              class="utk-hours--listing--item--hours">@php(LibCal::the_hours_by_lid(225))</span></a>
+          <div class="utk-hours--listing--item--meta--links">
+            <div class="utk-hours--listing--item--meta--links--spaces"><a
+                href="https://www.lib.utk.edu/hours/spaces/quiet-study-area/"><img alt="Quiet Study Area"
+                                                                                   src="https://www.lib.utk.edu/wp-content/blogs.dir/44/files/2019/12/276375_20191203_Night_Study_006-199x123.jpg"></a><a
+                href="https://www.lib.utk.edu/hours/spaces/collaborative-space/"><img alt="Collaborative Space"
+                                                                                      src="https://www.lib.utk.edu/wp-content/blogs.dir/44/files/2020/01/group6-199x123.jpg"></a><a
+                href="https://www.lib.utk.edu/hours/spaces/?fwp_location=pendergrass">View <br>Spaces <span
+                  class="sr-only">at pendergrass</span></a></div>
+          </div>
+        </div>
+      </li>
+      <li class="utk-hours--listing--item "><a href="https://lib.utk.edu/music" class="library-open">
+          <figure><img
+              src="/wp-content/themes/utk_lib_wp_theme/resources/assets/react/header/d9763b4914218a8fb970d6545997d543.jpg"
+              alt="George F. DeVine Music Library"></figure>
+        </a>
+        <div class="utk-hours--listing--item--meta"><a href="https://lib.utk.edu/music" class="library-open"><span
+              class="library-title">DeVine</span><span class="library-subtitle">Music Library</span><span
+              class="utk-hours--listing--item--hours">@php(LibCal::the_hours_by_lid(226))</span></a>
+          <div class="utk-hours--listing--item--meta--links">
+            <div class="utk-hours--listing--item--meta--links--spaces"><a
+                href="https://www.lib.utk.edu/hours/spaces/g017-study-room-g017-natalie-l-haslam-music-center/"><img
+                  alt="G017 Study Room (G017 Natalie L. Haslam Music Center)"
+                  src="https://www.lib.utk.edu/wp-content/blogs.dir/44/files/2019/12/MUSLIB_G017-StudyRoom-199x123.jpg"></a><a
+                href="https://www.lib.utk.edu/hours/spaces/group-study-tables-music-devine/"><img
+                  alt="Group Study Tables"
+                  src="https://www.lib.utk.edu/wp-content/blogs.dir/44/files/2019/12/MUSLIB_GroupStudyTables4-199x123.jpg"></a><a
+                href="https://www.lib.utk.edu/hours/spaces/?fwp_location=devine">View <br>Spaces <span
+                  class="sr-only">at devine</span></a></div>
+          </div>
+        </div>
+      </li>
+      <li class="utk-hours--listing--item "><a href="https://lib.utk.edu/request/storage" class="library-open">
+          <figure><img
+              src="/wp-content/themes/utk_lib_wp_theme/resources/assets/react/header/dff1a40b70fc66eaf0abdf8f6ce372d2.jpg"
+              alt="James D. Hoskins Library"></figure>
+        </a>
+        <div class="utk-hours--listing--item--meta"><a href="https://lib.utk.edu/request/storage"
+                                                       class="library-open"><span
+              class="library-title">Hoskins</span><span
+              class="library-subtitle">Storage &amp; Reading Room</span><span
+              class="utk-hours--listing--item--hours">@php(LibCal::the_hours_by_lid(227))</span></a>
+          <div class="utk-hours--listing--item--meta--links">
+            <div class="utk-hours--listing--item--meta--links--spaces"><a
+                href="https://www.lib.utk.edu/hours/spaces/storage-and-map-reading-room/"><img
+                  alt="Storage and Map Reading Room"
+                  src="https://www.lib.utk.edu/wp-content/blogs.dir/44/files/2019/12/hoskin-map-storaging-room-199x123.jpg"></a><a
+                href="https://www.lib.utk.edu/hours/spaces/?fwp_location=hoskins">View <br>Spaces <span
+                  class="sr-only">at hoskins</span></a></div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
 </div>
